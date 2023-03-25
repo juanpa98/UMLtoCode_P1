f2b9913ca44c954af62f530c1903b6923aff2252
juanpa98 <114074538+juanpa98@users.noreply.github.com>
2023-03-25 10:48:32 +0100

    Agregar archivos de proyecto.

---
+++ UMLtoCode_P1/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UMLtoCode_P1
+{
+    public class Account
+    {
+        List<Transaction> Transactions;
+        private string IBAN;
+        protected float Amount;
+        public bool Transaction(string transaction)
+        {
+            return true;
+        }
+    }
+    public struct Transaction
+    {
+        public string From;
+        public string To;
+        public float Amount;
+        public DateTime Request;
+
+
+    }
+    public class Investing : Account
+    {
+        public float Interest;
+        public void Applyinterest()
+        {
+
+        }
+
+    }
+    public class Saving : Account
+    {
+        private float ProtectedAmount;
+        public void LiberateSaving()
+        {
+
+        }
+
+    }
+}
---
+++ UMLtoCode_P1/Persona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace UMLtoCode_P1
+{
+    public class Persona
+    {
+        List<Address> Adresses;
+        public int ID;
+        public string Name;
+        protected string Phone;
+        public string Email;
+    }
+    public struct Address
+    {
+        public string Street;
+        public string City;
+        public string State;
+        public int PostalCode;
+        public string Country;
+        public bool Validate;
+    }
+    public class Employee : Persona
+    {
+        public Departament Departament;
+        private float Salary;
+    }
+
+    public enum Departament
+    {
+        finance,
+        sales
+
+    }
+
+    public class Customer : Persona
+    {
+        public List<Account> Acounts;
+        public DateTime Registartion;
+        private int CreditScore;
+
+    }
+}
